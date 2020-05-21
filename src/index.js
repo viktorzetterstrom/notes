9009e20dac7b8c7e509ff78875c7a77949f4cd48
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-05-21 12:59:29 +0200

    move commands to own folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const writeNote = require("./write-note");
-const openNotes = require("./open-notes");
-const printHelp = require("./print-help");
+const writeNote = require("./commands/write-note");
+const openNotes = require("./commands/open-notes");
+const printHelp = require("./commands/print-help");
 
 const [command, note] = process.argv.slice(2);
 
